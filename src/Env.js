635078a3478d26074f70938cc1998757616a8e0e
Oleg <oleg.pav.m@gmail.com>
2018-07-03 19:28:14 +0300

    Update ENV.

--- src/Env.js
+++ src/Env.js
@@ -1,7 +1,7 @@
 // Описание переменных окружения смотри в Readme.
 const env = process.env;
 const Moments = require('./Moments');
-const minute = +(Moments.oneMinute);
+const minute = +Moments.oneMinute;
 
 module.exports = {
     MONGO_CONNECT_STRING: env.MONGO_CONNECT_STRING || 'mongodb://mongo/admin',
@@ -10,6 +10,5 @@ module.exports = {
     BLOCKCHAIN_NODE_ADDRESS: env.BLOCKCHAIN_NODE_ADDRESS || 'wss://ws.golos.io',
     METRICS_HOST: env.METRICS_HOST || 'localhost',
     METRICS_PORT: env.METRICS_PORT || 8125,
-    GATE_SERVER_PORT: env.GATE_SERVER_PORT || 8090,
-    GATE_SERVER_TIMEOUT: env.GATE_SERVER_TIMEOUT || minute,
+    GATE_LISTEN_PORT: env.GATE_SERVER_PORT || 8080,
 };
