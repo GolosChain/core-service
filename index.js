a2b482ea0bb32ee88ec39aca66b3559227224187
Oleg <oleg.pav.m@gmail.com>
2018-07-23 14:03:02 +0300

    Update package name + fix unused require.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
     Logger: require('./src/Logger'),
     Moments: require('./src/Moments'),
     Stats: require('./src/Stats'),
-    ServiceAliasEnv: require('./src/ServiceAliasEnv'),
     service: {
         Basic: require('./src/service/Basic'),
         BlockSubscribe: require('./src/service/BlockSubscribe'),
