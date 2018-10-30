1623f350969e7104ef19673bfc37f97b7383ac03
Oleg <oleg.pav.m@gmail.com>
2018-10-30 19:57:56 +0300

    MongoBigNum schema.

---
+++ src/types/MongoBigNum.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const BigNum = require('./BigNum');
+
+/**
+ * Добавляет возможность использовать BigNum для хранения данных в MongoDB.
+ * Подключение типа происходит автоматически при подключении этого файла.
+ */
+class MongoBigNumSchema extends mongoose.SchemaType {
+    cast(value) {
+        return new BigNum(value);
+    }
+}
+
+mongoose.Schema.Types.MongoBigNum = MongoBigNumSchema;
