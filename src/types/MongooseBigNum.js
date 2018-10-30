7a11ca3195bd838c62f0f28acf72dc4561324b64
Oleg <oleg.pav.m@gmail.com>
2018-10-30 18:17:55 +0300

    MongooseBigNum type.

---
+++ src/types/MongooseBigNum.js
@@ -0,0 +1,21 @@
+/**
+ * Данный код позволяет сохранять тип BigNum в MongoDB через схемы.
+ * Для указания типа схемы достаточно указать тип MongoDB.types.BigNumber.
+ */
+
+const mongoose = require('mongoose');
+const BigNumber = require('./BigNum');
+
+// Этот вызов автоматически подключает оригинальный тип.
+const BigNumberSchema = require('mongoose-bignumber');
+
+/**
+ * Переопределяем конструктор типа mongoose-bignumber
+ * для того чтобы получить возможность работать с нашим
+ * собственным типом-оберткой.
+ */
+mongoose.Types.BigNumber.constructor = function(...args) {
+    const original = mongoose.Types.BigNumber.constructor.apply(this, args);
+
+    return new BigNumber(original);
+};
