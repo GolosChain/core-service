27a71035a2755f228bc5e5fc1e5f4546a285ed70
Oleg <oleg.pav.m@gmail.com>
2019-02-05 14:33:58 +0300

    #51 Add startMongoBeforeBoot method.

--- src/services/BasicMain.js
+++ src/services/BasicMain.js
@@ -1,5 +1,7 @@
 const Basic = require('./Basic');
 const ServiceMeta = require('../utils/ServiceMeta');
+const MongoDB = require('../services/MongoDB');
+const Logger = require('../utils/Logger');
 
 /**
  * Базовый класс главного класса приложения.
@@ -28,16 +30,22 @@ class BasicMain extends Basic {
         this._stats = stats;
         this.stopOnExit();
         this.throwOnUnhandledPromiseRejection();
+
+        this._startMongoBeforeBoot = false;
     }
 
     async start() {
+        await this._tryStartMongoBeforeBoot();
         await this.boot();
         await this.startNested();
+        this._tryIncludeMongoToNested();
+
         this._stats.increment(`${ServiceMeta.get('name')}:main_service_start`);
     }
 
     async stop() {
         await this.stopNested();
+
         this._stats.increment(`${ServiceMeta.get('name')}:main_service_stop`);
         process.exit(0);
     }
@@ -49,6 +57,46 @@ class BasicMain extends Basic {
     defineMeta(meta) {
         for (const key of Object.keys(meta)) {
             ServiceMeta.set(key, meta[key]);
+        }
+    }
+
+    /**
+     * Подключит и запустит сервис работы
+     * с базой данных MongoDB до запуска метода boot.
+     */
+    startMongoBeforeBoot() {
+        this._mongoDb = new MongoDB();
+        this._startMongoBeforeBoot = true;
+    }
+
+    async _tryStartMongoBeforeBoot() {
+        if (this._startMongoBeforeBoot) {
+            Logger.info(`Start MongoDB...`);
+            await this._mongoDb.start();
+            Logger.info(`The MongoDB done!`);
+
+            this._tryExcludeMongoFromNested();
+        }
+    }
+
+    _tryExcludeMongoFromNested() {
+        let forRemove = null;
+
+        for (let i = 0; i < this._nestedServices.length; i++) {
+            if (this._nestedServices[i] instanceof MongoDB) {
+                Logger.warn('Exclude MongoDB from nested services - startMongoBeforeBoot used');
+                forRemove = i;
+            }
+        }
+
+        if (forRemove !== null) {
+            this._nestedServices.slice(forRemove, 1);
+        }
+    }
+
+    _tryIncludeMongoToNested() {
+        if (this._startMongoBeforeBoot) {
+            this._nestedServices.unshift(this._mongoDb);
         }
     }
 }
