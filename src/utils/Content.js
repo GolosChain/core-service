50952d55da0816d185c4ceaf87a347bbc8862c67
Oleg <oleg.pav.m@gmail.com>
2019-02-12 19:23:24 +0300

    #53 Make more trim + docs.

--- src/utils/Content.js
+++ src/utils/Content.js
@@ -1,6 +1,15 @@
 const sanitizer = require('sanitize-html');
 
+/**
+ * Утилита для работы с контентом.
+ */
 class Content {
+    /**
+     * Очистить текст контента от лишних тегов.
+     * Также заменяет тег H1 на H2 для более адекватного SEO.
+     * @param {string} text Целевой текст.
+     * @return {string} Результат.
+     */
     sanitize(text) {
         return sanitizer(text, {
             allowedTags: [
@@ -35,6 +44,13 @@ class Content {
         });
     }
 
+    /**
+     * Очищает текст для соответствия виду превью - удаляет все теги,
+     * умно обрезает до нужной длинны и умно добавляет в конце троеточие.
+     * @param {string} text Целевой текст.
+     * @param {number} maxSize Максимальная длина результирующей строки.
+     * @return {string} Результат.
+     */
     sanitizePreview(text, maxSize) {
         const sanitized = sanitizer(text, {
             allowedTags: [],
@@ -43,9 +59,15 @@ class Content {
         return this.smartTrim(sanitized, maxSize);
     }
 
+    /**
+     * Умно обрезает строку до нужной длинны и умно добавляет в конце троеточие.
+     * @param {string} text Целевой текст.
+     * @param {number} maxSize Максимальная длина результирующей строки.
+     * @return {string} Результат.
+     */
     smartTrim(text, maxSize) {
         if (text.length <= maxSize) {
-            return text;
+            return text.trim();
         }
 
         const hardTrim = this.hardTrim(text, maxSize);
@@ -54,21 +76,42 @@ class Content {
         return this.makeEllipses(softTrim);
     }
 
-    softTrimByDot(text) {
+    /**
+     * Пытается умно обрезать строку до нужной длинны по точке.
+     * @param {string} text Целевой текст.
+     * @param {number} tolerance Уровень толлерантности при фильтрации.
+     * @return {string} Результат.
+     */
+    softTrimByDot(text, tolerance = 0.86) {
         const dotIndex = text.lastIndexOf('. ');
-        const softLimit = Math.round(text.length * 0.86);
+        const softLimit = Math.round(text.length * tolerance);
 
         if (dotIndex > softLimit) {
-            return text.substring(0, dotIndex + 1);
+            return text.substring(0, dotIndex + 1).trim();
         } else {
-            return text;
+            return text.trim();
         }
     }
 
+    /**
+     * Жестко обрезает строку по лимиту
+     * и зачищает лишние пустые символы по краям.
+     * @param {string} text Целевой текст.
+     * @param {number} maxSize Максимальная длина результирующей строки.
+     * @return {string} Результат.
+     */
     hardTrim(text, maxSize) {
-        return text.substring(0, maxSize).trim();
+        return text
+            .trim()
+            .substring(0, maxSize)
+            .trim();
     }
 
+    /**
+     * Умно добавляет в конец строки троеточие.
+     * @param {string} text Целевой текст.
+     * @return {string} Результат.
+     */
     makeEllipses(text) {
         return text.replace(/[,!?]?\s+[^\s]+$/, '…');
     }
