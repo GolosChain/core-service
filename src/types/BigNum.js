604634b8fc581fca46ed63d4cc16696165402905
Oleg <oleg.pav.m@gmail.com>
2019-03-13 19:22:02 +0300

    #36 Improved BigNum.

--- src/types/BigNum.js
+++ src/types/BigNum.js
@@ -1,102 +1,202 @@
-const BN = require('bignumber.js');
+const BigNumber = require('bignumber.js');
 
 /**
- * Обертка над библиотекой bignumber.js.
+ * Обертка над библиотекой bignumber.js, доступные
+ * методы необходимо смотреть в соответствующей документации.
+ * Переопределены только необходимые для переопределения методы.
+ *
  * В отличии от оригинала использует более гибкий
  * конструктор и вызовы арифметических методов,
  * позволяя передавать не поддерживаемые в оригинале
  * числоподобные значения, например содержащие постфикс
- * вида '1000 gbg'. Использование parseFloat искажает
+ * вида '1000 coin'. Использование parseFloat искажает
  * оригинальное значение для очень больних чисел,
  * но эта обертка позволяет работать с ними без потерь.
- *
- * Является конечным классом, наследование не предполагается.
  */
-class BigNum {
+class BigNum extends BigNumber {
     /**
+     * @deprecated
      * Возвращает класс оригинальной библиотеки bignumber.js.
      * @returns {{BigNumber}} Класс.
      */
     static OriginalBigNumber() {
-        return BN;
+        return BigNumber;
+    }
+
+    static clone(config) {
+        const original = BigNum;
+        const result = class BigNum extends original {};
+
+        if (config) {
+            result.config(config);
+        }
+
+        return result;
+    }
+
+    static maximum(...args) {
+        return new BigNum(super.maximum(...args));
+    }
+
+    static max(...args) {
+        return new BigNum(super.max(...args));
+    }
+
+    static minimum(...args) {
+        return new BigNum(super.minimum(...args));
+    }
+
+    static min(...args) {
+        return new BigNum(super.min(...args));
+    }
+
+    static random(...args) {
+        return new BigNum(super.random(...args));
     }
 
     /**
      * Конструктор, возвращающий прокси над bignumber.js.
-     * @param {number|string|BigInt|BN} value Любое числоподобное
+     * @param {number|string|BigInt|BigNumber} value Любое числоподобное
      * значение, включая значения с префиксами и постфиксами.
-     * @returns {Proxy} Прокси.
+     * @param {number} [base] Система счисления (от 2 до 36).
      */
-    constructor(value) {
-        this._value = this._convertValue(value);
+    constructor(value, base) {
+        super(); // create context only
+        super.constructor(this._convertValue(value), base);
+    }
 
-        return new Proxy(this._value, {
-            get: (target, property) => {
-                switch (property) {
-                    case 'comparedTo':
-                    case 'dividedBy':
-                    case 'div':
-                    case 'dividedToIntegerBy':
-                    case 'idiv':
-                    case 'exponentiatedBy':
-                    case 'pow':
-                    case 'isEqualTo':
-                    case 'eq':
-                    case 'isGreaterThan':
-                    case 'gt':
-                    case 'isGreaterThanOrEqualTo':
-                    case 'gte':
-                    case 'isLessThan':
-                    case 'lt':
-                    case 'isLessThanOrEqualTo':
-                    case 'lte':
-                    case 'minus':
-                    case 'modulo':
-                    case 'mod':
-                    case 'multipliedBy':
-                    case 'times':
-                    case 'plus':
-                    case 'squareRoot':
-                    case 'sqrt':
-                        return this._makeCallWrapper(target, property);
-                    case 'rawValue':
-                        return this.rawValue.bind(this);
-                    case 'toBSON':
-                        return this.toBSON.bind(this);
-                    default:
-                        return target[property];
-                }
-            },
-        });
+    absoluteValue() {
+        return new BigNum(super.absoluteValue());
+    }
+
+    abs() {
+        return new BigNum(super.abs());
+    }
+
+    decimalPlaces(...args) {
+        const result = super.decimalPlaces(...args);
+
+        if (typeof result === 'number') {
+            return result;
+        } else {
+            return new BigNum(result);
+        }
+    }
+
+    dp(...args) {
+        const result = super.dp(...args);
+
+        if (typeof result === 'number') {
+            return result;
+        } else {
+            return new BigNum(result);
+        }
+    }
+
+    dividedBy(...args) {
+        return new BigNum(super.dividedBy(...args));
+    }
+
+    div(...args) {
+        return new BigNum(super.div(...args));
+    }
+
+    dividedToIntegerBy(...args) {
+        return new BigNum(super.dividedToIntegerBy(...args));
+    }
+
+    idiv(...args) {
+        return new BigNum(super.idiv(...args));
+    }
+
+    exponentiatedBy(...args) {
+        return new BigNum(super.exponentiatedBy(...args));
+    }
+
+    pow(...args) {
+        return new BigNum(super.pow(...args));
+    }
+
+    integerValue(...args) {
+        return new BigNum(super.integerValue(...args));
+    }
+
+    minus(...args) {
+        return new BigNum(super.minus(...args));
+    }
+
+    modulo(...args) {
+        return new BigNum(super.modulo(...args));
+    }
+
+    mod(...args) {
+        return new BigNum(super.mod(...args));
+    }
+
+    multipliedBy(...args) {
+        return new BigNum(super.multipliedBy(...args));
+    }
+
+    times(...args) {
+        return new BigNum(super.times(...args));
+    }
+
+    negated() {
+        return new BigNum(super.negated());
+    }
+
+    plus(...args) {
+        return new BigNum(super.plus(...args));
+    }
+
+    precision(...args) {
+        return new BigNum(super.precision(...args));
+    }
+
+    sd(...args) {
+        return new BigNum(super.sd(...args));
+    }
+
+    shiftedBy(...args) {
+        return new BigNum(super.shiftedBy(...args));
+    }
+
+    squareRoot() {
+        return new BigNum(super.squareRoot());
+    }
+
+    sqrt() {
+        return new BigNum(super.sqrt());
     }
 
     /**
-     * @returns {BN} Оригинальный инстанс bignumber.js.
+     * @deprecated
+     * @returns {BigNumber} Оригинальный инстанс bignumber.js.
      */
     rawValue() {
-        return this._value;
+        return this;
     }
 
     /**
      * @returns {string} Значение, пригодное для BSON.
      */
     toBSON() {
-        return this._value.toString();
+        return this.toString();
     }
 
     _convertValue(value) {
-        if (value instanceof BN) {
+        if (value instanceof BigNumber) {
             return value;
         }
 
         if (typeof value === 'number') {
-            return new BN(value);
+            return new BigNumber(value);
         }
 
         if (typeof value === 'string') {
             value = value.trim();
 
-            const original = new BN(value);
+            const original = new BigNumber(value);
 
             if (!original.isNaN()) {
                 return original;
@@ -110,31 +210,13 @@ class BigNum {
             const matched = value.match(new RegExp(check));
 
             if (matched) {
-                return new BN(matched[0]);
+                return new BigNumber(matched[0]);
             } else {
                 return original;
             }
         }
 
-        return new BN(value);
-    }
-
-    _makeCallWrapper(target, property) {
-        return (...rawArgs) => {
-            const convertedArgs = [];
-
-            for (let raw of rawArgs) {
-                convertedArgs.push(this._convertValue(raw));
-            }
-
-            const result = target[property].apply(this._value, convertedArgs);
-
-            if (result instanceof BN) {
-                return new BigNum(result);
-            }
-
-            return result;
-        };
+        return new BigNumber(value);
     }
 }
 
