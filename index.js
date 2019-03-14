4f3fd733b977d91b73a049910dce51f6155c25d2
Oleg <oleg.pav.m@gmail.com>
2019-03-14 12:54:59 +0300

    #63 Help for IDE class linking.

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
+// Help for IDE class linking
+const ControllersBasic = require('./src/controllers/Basic');
+
+const ServicesBasic = require('./src/services/Basic');
+const ServicesBasicMain = require('./src/services/BasicMain');
+const ServicesBlockSubscribe = require('./src/services/BlockSubscribe');
+const ServicesMongoDB = require('./src/services/MongoDB');
+const ServicesConnector = require('./src/services/Connector');
+
+const UtilsLogger = require('./src/utils/Logger');
+const UtilsMoments = require('./src/utils/Moments');
+const UtilsTemplate = require('./src/utils/Template');
+const UtilsRpcObject = require('./src/utils/RpcObject');
+const utilsStatsClient = require('./src/utils/statsClient');
+const UtilsServiceMeta = require('./src/utils/ServiceMeta');
+const UtilsContent = require('./src/utils/Content');
+const utilsDefaultStarter = require('./src/utils/defaultStarter');
+const UtilsGateClient = require('./src/utils/GateClient');
+
+const TypesBigNum = require('./src/types/BigNum');
+const TypesMongoBigNum = require('./src/types/MongoBigNum');
+
+const dataEnv = require('./src/data/env');
+
+// Export public classes
 module.exports = {
     controllers: {
-        Basic: require('./src/controllers/Basic'),
+        Basic: ControllersBasic,
     },
     services: {
-        Basic: require('./src/services/Basic'),
-        BasicMain: require('./src/services/BasicMain'),
-        BlockSubscribe: require('./src/services/BlockSubscribe'),
-        MongoDB: require('./src/services/MongoDB'),
-        Connector: require('./src/services/Connector'),
+        Basic: ServicesBasic,
+        BasicMain: ServicesBasicMain,
+        BlockSubscribe: ServicesBlockSubscribe,
+        MongoDB: ServicesMongoDB,
+        Connector: ServicesConnector,
     },
     utils: {
-        Logger: require('./src/utils/Logger'),
-        Moments: require('./src/utils/Moments'),
-        Template: require('./src/utils/Template'),
-        RpcObject: require('./src/utils/RpcObject'),
-        statsClient: require('./src/utils/statsClient'),
-        ServiceMeta: require('./src/utils/ServiceMeta'),
-        Content: require('./src/utils/Content'),
-        defaultStarter: require('./src/utils/defaultStarter'),
-        GateClient: require('./src/utils/GateClient'),
+        Logger: UtilsLogger,
+        Moments: UtilsMoments,
+        Template: UtilsTemplate,
+        RpcObject: UtilsRpcObject,
+        statsClient: utilsStatsClient,
+        ServiceMeta: UtilsServiceMeta,
+        Content: UtilsContent,
+        defaultStarter: utilsDefaultStarter,
+        GateClient: UtilsGateClient,
     },
     types: {
-        BigNum: require('./src/types/BigNum'),
-        MongoBigNum: require('./src/types/MongoBigNum'),
+        BigNum: TypesBigNum,
+        MongoBigNum: TypesMongoBigNum,
     },
     data: {
-        env: require('./src/data/env'),
+        env: dataEnv,
     },
 };
