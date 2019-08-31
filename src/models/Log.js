8bc19104dcc31f518e267f97d3cc75674b67ef27
IvanBlacky <tcherivan@gmail.com>
2019-08-31 22:10:31 +0300

    Feat: Logger: add required index

--- src/models/Log.js
+++ src/models/Log.js
@@ -1,13 +1,25 @@
 const MongoDB = require('../services/MongoDB');
 
-module.exports = MongoDB.makeModel('Log', {
-    entry: {
-        type: String,
-        required: true,
+module.exports = MongoDB.makeModel(
+    'Log',
+    {
+        entry: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'log',
+            enum: ['log', 'info', 'warn', 'error'],
+        },
     },
-    type: {
-        type: String,
-        default: 'log',
-        enum: ['log', 'info', 'warn', 'error'],
-    },
-});
+    {
+        index: [
+            {
+                fields: {
+                    type: 1,
+                },
+            },
+        ],
+    }
+);
