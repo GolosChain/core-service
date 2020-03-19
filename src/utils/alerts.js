efb90eecd1b94720804a6b8abb989e34f61ff0a0
Ilya Lebedev <bacherko@gmail.com>
2020-03-19 19:45:22 +0300

    fix: fix slack alert (#180)
    * fix: fix slack alert
    
    * 3.59.2

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -2,18 +2,20 @@ const fetch = require('node-fetch');
 const env = require('../data/env');
 const globalData = require('../data/data');
 
-const ALLOWED_TYPES = ['warning', 'error'];
+const ALLOWED_TYPES = ['warning', 'error', 'danger'];
 
 /**
  * Send alert message.
  *
- * @param {'warning'|'error'} [type] (default: 'warning')
+ * @param {'warning'|'error'|'danger'} [type] (default: 'warning')
  * @param {string} title
  * @param {string} text
+ * @param {string} [prefix]
+ * @param {string} [suffix]
  * @param {number} [timestamp]
  */
 
-function sendAlert({ type = 'warning', title, text, timestamp }) {
+function sendAlert({ type = 'warning', title, text, prefix, suffix, timestamp }) {
     if (!ALLOWED_TYPES.includes(type)) {
         throw new Error('Invalid type');
     }
@@ -22,14 +24,18 @@ function sendAlert({ type = 'warning', title, text, timestamp }) {
         return;
     }
 
-    _sendAlert({ type, title, text, timestamp }).catch(err => {
+    if (type === 'error') {
+        type = 'danger';
+    }
+
+    _sendAlert({ type, title, text, prefix, suffix, timestamp }).catch(err => {
         Logger.warn('Sending alert failed:', err);
     });
 }
 
-async function _sendAlert({ type, title, text, timestamp }) {
+async function _sendAlert({ type, title, text, prefix, suffix, timestamp }) {
     const data = {
-        text: `Service "${globalData.serviceName || 'unknown'}"`,
+        text: `${prefix || ''}Service "${globalData.serviceName || 'unknown'}${suffix || ''}"`,
         attachments: [
             {
                 color: type,
